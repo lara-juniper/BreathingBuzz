ac46634848e9a43be857c3e77ad195e7582550d7
Lara Orlandic <lara.orlandic@gmail.com>
2019-03-20 22:16:38 -0400

    added welcome activity

---
+++ app/src/main/java/com/adafruit/bluefruit/le/connect/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package com.adafruit.bluefruit.le.connect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.adafruit.bluefruit.le.connect.app.MainActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+    }
+}
